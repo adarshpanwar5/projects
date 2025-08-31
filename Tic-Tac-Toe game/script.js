7f9bf570a4bfa5ab2093a86af20d75ae3cb4700d
adarshpanwar5 <130700950+adarshpanwar5@users.noreply.github.com>
2025-08-31 12:12:14 +0530

    Add files via upload

---
+++ Tic-Tac-Toe game/script.js
@@ -0,0 +1,101 @@
+let boxes = document.querySelectorAll(".box");
+let reset_Btn = document.querySelector("#reset-btn");
+let newGameBtn=document.querySelector("#new-btn");
+let msg_container=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+
+
+
+let turnO=true;  //playerX,playerO
+
+const winPatterns =[
+[0,1,2],
+[0,3,6],
+[0,4,8],
+[1,4,7],
+[2,5,8],
+[2,4,6],
+[3,4,5],
+[6,7,8]
+];
+
+
+
+
+
+boxes.forEach((box) =>{
+  box.addEventListener("click",()=>{ 
+ //  console.log("box was clicked");
+     if(turnO){  
+        //player O
+        box.innerText="O";
+        turnO=false; 
+        box.style.color="green";
+       
+     }
+     else{
+        //player X
+        box.innerText="X";
+        turnO=true;
+        box.style.color="blue";
+     }
+     box.disabled=true;
+    
+
+     checkWinner();
+   
+    
+
+   })
+ });
+
+
+
+ const checkWinner = () =>{
+    for(let pattern of winPatterns){
+            let pos1val=boxes[pattern[0]].innerText;
+            let pos2val=boxes[pattern[1]].innerText;
+            let pos3val=boxes[pattern[2]].innerText;
+
+            if(pos1val!="" && pos2val!="" && pos3val!=""){
+                if(pos1val === pos2val && pos2val === pos3val){
+                   // console.log("winner",pos1val);
+                    showWinner(pos1val);
+                }
+            }
+    }
+  };
+
+
+  const showWinner=(winner)=>{
+    msg.innerText=`Congretulation, winner is ${winner}`;
+    msg_container.classList.remove("hide");
+    disable_Boxes();
+
+  };
+
+  const disable_Boxes =()=>{
+    for(let box of boxes){
+      box.disabled=true;
+    }
+  };
+ 
+
+  
+  const resetGame =()=>{
+    turnO= true;
+    enableBoxes();
+    msg_container.classList.add("hide");
+  
+  }
+  const enableBoxes =()=>{
+    for(let box of boxes){
+      box.disabled=false;
+      box.innerText="";
+    }
+  };
+  
+  
+  newGameBtn.addEventListener("click",resetGame);
+  reset_Btn.addEventListener("click",resetGame);
+  
